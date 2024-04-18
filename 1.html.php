830df96093c94491e5253b7b82db2c738e93ece5
hung0221 <155549020+hung0221@users.noreply.github.com>
2024-04-18 21:21:39 +0800

    1.html.php

---
+++ 1.html.php
@@ -0,0 +1,10 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!";  ?> // 輸出 "Hello world!" 字串
+        <h1>html</h1>
+        <?php echo "Hello world!";  ?> // 輸出 "Hello world!" 字串
+        <h1>html</h1>
+        <?php echo "Hello world!";  ?> // 輸出 "Hello world!" 字串
+    </body>
+</html>
